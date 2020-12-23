cbd1357a4c027ad23133f00a7d045247a836e19c
anacamargos <alcvlara@sga.pucminas.br>
2020-12-23 18:51:07 -0300

    Updating home view controller

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/Home/HomeViewController.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/Home/HomeViewController.swift
@@ -9,4 +9,15 @@ import UIKit
 
 final class HomeViewController: UIViewController {
     
+    // MARK: - View Components
+    
+    weak var contentView: HomeContentViewProtocol?
+    
+    // MARK: - View Controller Lifecycle
+    
+    override func loadView() {
+        super.loadView()
+        view = HomeContentView()
+        contentView = view as? HomeContentViewProtocol
+    }
 }
