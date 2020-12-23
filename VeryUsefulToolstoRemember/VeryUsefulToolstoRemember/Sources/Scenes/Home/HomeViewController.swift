5de557c3ae8563d0814327bbe6e66e24232e63fd
anacamargos <alcvlara@sga.pucminas.br>
2020-12-23 19:04:58 -0300

    Adding navigation bar to home view controller

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/Home/HomeViewController.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/Home/HomeViewController.swift
@@ -15,9 +15,24 @@ final class HomeViewController: UIViewController {
     
     // MARK: - View Controller Lifecycle
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavigationBar()
+    }
+    
     override func loadView() {
         super.loadView()
         view = HomeContentView()
         contentView = view as? HomeContentViewProtocol
+    }
+    
+    private func configureNavigationBar() {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "VUTTR"
+        
+        let search = UISearchController(searchResultsController: nil)
+//        search.searchResultsUpdater = self
+        self.navigationItem.searchController = search
     }
 }
