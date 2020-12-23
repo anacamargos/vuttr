f0538e71eace8d3857856ce3c3098eaa016360b5
anacamargos <alcvlara@sga.pucminas.br>
2020-12-23 18:45:20 -0300

    Updating home content view

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/Home/Views/HomeContentView.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/Home/Views/HomeContentView.swift
@@ -11,4 +11,35 @@ protocol HomeContentViewProtocol: AnyObject {}
 
 final class HomeContentView: CodedView {
     
+    // MARK: - Initializers
+    
+    override init(
+        frame: CGRect = .zero
+    ) {
+        super.init(frame: frame)
+        configureView()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Override Methods
+    
+    override func addSubviews() {
+    }
+    
+    override func constrainSubviews() {
+    }
+    
+    // MARK: - Private Methods
+    
+    private func configureView() {
+        backgroundColor = .white
+    }
 }
+
+// MARK: - HomeContentViewProtocol
+
+extension HomeContentView: HomeContentViewProtocol {}
