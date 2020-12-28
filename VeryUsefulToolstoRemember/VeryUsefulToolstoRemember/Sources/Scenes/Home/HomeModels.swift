79e433fd4611913b0bfd61ec47ea8ce5132b2523
anacamargos <alcvlara@sga.pucminas.br>
2020-12-28 16:17:13 -0300

    Creating models for home

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/Home/HomeModels.swift
@@ -0,0 +1,31 @@
+//
+//  HomeModels.swift
+//  VeryUsefulToolstoRemember
+//
+//  Created by Ana Leticia Camargos on 28/12/20.
+//  Copyright © 2020 Ana Letícia Camargos. All rights reserved.
+//
+
+import Foundation
+
+enum Home {
+    
+    enum UsefulTools {
+        
+        enum ViewState {
+            case content(ViewData)
+            case loading
+            case error
+        }
+        
+        struct ViewData {
+            let tools: [Tool]
+        }
+        
+        struct Tool {
+            let title: String
+            let description: String
+            let tags: [String]
+        }
+    }
+}
