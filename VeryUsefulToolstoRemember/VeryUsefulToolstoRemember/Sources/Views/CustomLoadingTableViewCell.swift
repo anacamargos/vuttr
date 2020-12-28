c7d56d16be69d0fe04c06cda5d9f270539edc3b3
anacamargos <alcvlara@sga.pucminas.br>
2020-12-28 12:29:28 -0300

    Creating loading table view cell

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Views/CustomLoadingTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  CustomLoadingTableViewCell.swift
+//  VeryUsefulToolstoRemember
+//
+//  Created by Ana Leticia Camargos on 28/12/20.
+//  Copyright © 2020 Ana Letícia Camargos. All rights reserved.
+//
+
+import UIKit
+
+final class CustomLoadingTableViewCell: CodedTableViewCell {
+    
+    // MARK: - View Components
+    
+    private let loadingView: CustomLoadingView = CustomLoadingView()
+    
+    // MARK: - Override Methods
+    
+    override func addSubviews() {
+        addSubview(loadingView)
+    }
+    
+    override func constrainSubviews() {
+        backgroundColor = .clear
+        selectionStyle = .none
+        loadingView.fillSuperview()
+    }
+    
+    // MARK: - Public Methods
+    
+    func startLoading() {
+        loadingView.startLoading()
+    }
+}
