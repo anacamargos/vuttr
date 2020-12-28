769a84659c8bc005e6ce5c4ae15b46206523a202
anacamargos <alcvlara@sga.pucminas.br>
2020-12-28 12:31:46 -0300

    Creating new extension

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Extensions/NSObjectExtensions.swift
@@ -0,0 +1,20 @@
+//
+//  NSObjectExtensions.swift
+//  VeryUsefulToolstoRemember
+//
+//  Created by Ana Leticia Camargos on 28/12/20.
+//  Copyright © 2020 Ana Letícia Camargos. All rights reserved.
+//
+
+import Foundation
+
+extension NSObject {
+    
+    static var className: String {
+        return String(describing: self)
+    }
+    
+    var className: String {
+        return type(of: self).className
+    }
+}
