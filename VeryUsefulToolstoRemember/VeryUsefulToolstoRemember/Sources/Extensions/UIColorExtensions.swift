f73c442bdf7ece54d5d72b47a2f3edd952ebc153
anacamargos <alcvlara@sga.pucminas.br>
2020-12-28 16:36:48 -0300

    Adding new color

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Extensions/UIColorExtensions.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Extensions/UIColorExtensions.swift
@@ -13,4 +13,8 @@ extension UIColor {
     @nonobjc class var ink: UIColor {
         .init(red: 23.0 / 255.0, green: 12.0 / 255.0, blue: 58.0 / 255.0, alpha: 1.0)
     }
+    
+    @nonobjc class var darkestWhite: UIColor {
+        .init(red: 235.0 / 255.0, green: 234.0 / 255.0, blue: 237.0 / 255.0, alpha: 1.0)
+    }
 }
