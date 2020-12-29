007a9f4c95fcd31d22e7f4b4f73454e357a42169
anacamargos <alcvlara@sga.pucminas.br>
2020-12-29 11:08:36 -0300

    Creating tests for cell

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Tests/Scenes/Home/Views/UsefulToolCellTests.swift
@@ -0,0 +1,38 @@
+//
+//  UsefulToolCellTests.swift
+//  VeryUsefulToolstoRememberTests
+//
+//  Created by Ana Leticia Camargos on 29/12/20.
+//  Copyright © 2020 Ana Letícia Camargos. All rights reserved.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import VeryUsefulToolstoRemember
+
+final class UsefulToolCellTests: XCTestCase {
+
+    func test_usefulToolCell() {
+        let view = makeView()
+        view.setupViewData(.mock)
+        assertSnapshot(matching: view, as: .image)
+    }
+    
+    // MARK: - Test Helpers
+    
+    private func makeView() -> UsefulToolCell {
+        let view = UsefulToolCell()
+        view.frame = .init(origin: .zero, size: CGSize(width: 300, height: 150))
+        return view
+    }
+}
+
+extension Home.UsefulTools.Tool {
+    static var mock: Home.UsefulTools.Tool {
+        .init(
+            title: "Notion",
+            description: "All in one too to organize teams and ideas. Write, plan, collaborate, and get organized.",
+            tags: ["organization", "planning", "collaboration", "writing"]
+        )
+    }
+}
