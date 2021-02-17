c41e715f066823d1e1bbcb42c56f21cc68458053
anacamargos <alcvlara@sga.pucminas.br>
2021-02-17 18:07:46 -0300

    Creating new colors

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Extensions/UIColorExtensions.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Extensions/UIColorExtensions.swift
@@ -10,13 +10,51 @@ import UIKit
 
 extension UIColor {
     
+    // MARK: - White
+    
+    /// #FCFCFD
+    @nonobjc class var darkWhite: UIColor {
+        .init(red: 252.0 / 255.0, green: 252.0 / 255.0, blue: 253.0 / 255.0, alpha: 1.0)
+    }
+    
+    /// #F5F4F6
+    @nonobjc class var darkerWhite: UIColor {
+        .init(red: 245.0 / 255.0, green: 244.0 / 255.0, blue: 246.0 / 255.0, alpha: 1.0)
+    }
+    
+    /// #EBEAED
+    @nonobjc class var darkestWhite: UIColor {
+        .init(red: 235.0 / 255.0, green: 234.0 / 255.0, blue: 237.0 / 255.0, alpha: 1.0)
+    }
+    
+    /// #DEDCE1
+    @nonobjc class var mostDarkestWhite: UIColor {
+        .init(red: 222.0 / 255.0, green: 220.0 / 255.0, blue: 225.0 / 255.0, alpha: 1.0)
+    }
+    
+    // MARK: - Ink
+    
+    /// #170C3A
     @nonobjc class var ink: UIColor {
         .init(red: 23.0 / 255.0, green: 12.0 / 255.0, blue: 58.0 / 255.0, alpha: 1.0)
     }
     
-    @nonobjc class var darkestWhite: UIColor {
-        .init(red: 235.0 / 255.0, green: 234.0 / 255.0, blue: 237.0 / 255.0, alpha: 1.0)
+    /// #8F8A9B
+    @nonobjc class var lightInk: UIColor {
+        .init(red: 143.0 / 255.0, green: 138.0 / 255.0, blue: 155.0 / 255.0, alpha: 1.0)
     }
+    
+    /// #B1ADB9
+    @nonobjc class var lighterInk: UIColor {
+        .init(red: 177.0 / 255.0, green: 173.0 / 255.0, blue: 185.0 / 255.0, alpha: 1.0)
+    }
+    
+    /// #C7C4CD
+    @nonobjc class var lightestInk: UIColor {
+        .init(red: 199.0 / 255.0, green: 196.0 / 255.0, blue: 205.0 / 255.0, alpha: 1.0)
+    }
+    
+    // MARK: - Green
     
     @nonobjc class var green: UIColor {
         .init(red: 18.0 / 255.0, green: 219.0 / 255.0, blue: 137.0 / 255.0, alpha: 1.0)
