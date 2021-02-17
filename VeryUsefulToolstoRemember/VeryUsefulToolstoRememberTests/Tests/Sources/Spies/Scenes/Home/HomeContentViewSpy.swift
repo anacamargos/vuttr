c451e9b1915908c3a497d79af66b49d706bfffe6
anacamargos <alcvlara@sga.pucminas.br>
2021-02-17 19:07:01 -0300

    Moving files to new folder

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Tests/Sources/Spies/Scenes/Home/HomeContentViewSpy.swift
@@ -0,0 +1,18 @@
+//
+//  HomeContentViewSpy.swift
+//  VeryUsefulToolstoRememberTests
+//
+//  Created by Ana Leticia Camargos on 17/02/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import Foundation
+@testable import VeryUsefulToolstoRemember
+
+final class HomeContentViewSpy: HomeContentViewProtocol {
+    private(set) var setupUsefulToolsStatePassedViewStates = [Home.UsefulTools.ViewState]()
+    
+    func setupUsefulToolsState(_ viewState: Home.UsefulTools.ViewState) {
+        setupUsefulToolsStatePassedViewStates.append(viewState)
+    }
+}
