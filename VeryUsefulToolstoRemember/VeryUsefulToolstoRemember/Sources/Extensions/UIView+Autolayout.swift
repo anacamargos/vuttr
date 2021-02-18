62f5227526d78d50baeda982e145125dd5b0689c
anacamargos <alcvlara@sga.pucminas.br>
2021-02-18 17:46:17 -0300

    Adding new method that uses leading and trailing anchors

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Extensions/UIView+Autolayout.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Extensions/UIView+Autolayout.swift
@@ -25,9 +25,9 @@ extension UIView {
     @discardableResult
     func anchor(
         top: NSLayoutYAxisAnchor? = nil,
-        left: NSLayoutXAxisAnchor? = nil,
+        left: NSLayoutXAxisAnchor?,
         bottom: NSLayoutYAxisAnchor? = nil,
-        right: NSLayoutXAxisAnchor? = nil,
+        right: NSLayoutXAxisAnchor?,
         topConstant: CGFloat = 0,
         leftConstant: CGFloat = 0,
         bottomConstant: CGFloat = 0,
@@ -36,48 +36,101 @@ extension UIView {
         heightConstant: CGFloat = 0
     ) -> [NSLayoutConstraint] {
         translatesAutoresizingMaskIntoConstraints = false
-
+        
         var anchors = [NSLayoutConstraint]()
-
+        
         if let top = top {
             anchors.append(topAnchor.constraint(equalTo: top, constant: topConstant))
         }
-
+        
         if let left = left {
             anchors.append(leftAnchor.constraint(equalTo: left, constant: leftConstant))
         }
-
+        
         if let bottom = bottom {
             anchors.append(bottomAnchor.constraint(equalTo: bottom, constant: -bottomConstant))
         }
-
+        
         if let right = right {
             anchors.append(rightAnchor.constraint(equalTo: right, constant: -rightConstant))
         }
-
+        
         if widthConstant > 0 {
             anchors.append(widthAnchor.constraint(equalToConstant: widthConstant))
         }
-
+        
         if heightConstant > 0 {
             anchors.append(heightAnchor.constraint(equalToConstant: heightConstant))
         }
-
+        
         anchors.forEach({ $0.isActive = true })
-
+        
         return anchors
     }
+    
+    /// Add anchors from any side of the current view into the specified anchors and returns the newly added constraints.
+    ///
+    /// - Parameters:
+    ///   - top: current view's top anchor will be anchored into the specified anchor
+    ///   - leading: current view's leading anchor will be anchored into the specified anchor
+    ///   - bottom: current view's bottom anchor will be anchored into the specified anchor
+    ///   - trailing: current view's trailing anchor will be anchored into the specified anchor
+    ///   - topConstant: current view's top anchor margin
+    ///   - leftConstant: current view's left anchor margin
+    ///   - bottomConstant: current view's bottom anchor margin
+    ///   - rightConstant: current view's right anchor margin
+    ///   - widthConstant: current view's width
+    ///   - heightConstant: current view's height
+    /// - Returns: array of newly added constraints (if applicable).
+    @discardableResult
+    public func anchor(
+        top: NSLayoutYAxisAnchor? = nil,
+        leading: NSLayoutXAxisAnchor? = nil,
+        bottom: NSLayoutYAxisAnchor? = nil,
+        trailing: NSLayoutXAxisAnchor? = nil,
+        topConstant: CGFloat = 0,
+        leadingConstant: CGFloat = 0,
+        bottomConstant: CGFloat = 0,
+        trailingConstant: CGFloat = 0,
+        widthConstant: CGFloat = 0,
+        heightConstant: CGFloat = 0
+    ) -> [NSLayoutConstraint] {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        var anchors = anchor(
+            top: top,
+            left: nil,
+            bottom: bottom,
+            right: nil,
+            topConstant: topConstant,
+            bottomConstant: bottomConstant,
+            widthConstant: widthConstant,
+            heightConstant: heightConstant
+        )
+        if let leading = leading {
+            let leadingContraint = leadingAnchor.constraint(equalTo: leading, constant: leadingConstant)
+            anchors.append(leadingContraint)
+            leadingContraint.isActive = true
+        }
+        if let trailing = trailing {
+            let traillingConstraint = trailingAnchor.constraint(equalTo: trailing, constant: -trailingConstant)
+            anchors.append(traillingConstraint)
+            traillingConstraint.isActive = true
+        }
+        return anchors
+    }
+    
     
     /// makes the view to fill the entire superview.
     func fillSuperview() {
         anchor(
             top: superview?.topAnchor,
-            left: superview?.leftAnchor,
+            leading: superview?.leadingAnchor,
             bottom: superview?.bottomAnchor,
-            right: superview?.rightAnchor
+            trailing: superview?.trailingAnchor
         )
     }
-
+    
     /// Anchor center X into current view's superview with a constant margin value.
     ///
     /// - Parameter constant: constant of the anchor constraint (default is 0).
@@ -87,7 +140,7 @@ extension UIView {
             centerXAnchor.constraint(equalTo: anchor, constant: constant).isActive = true
         }
     }
-
+    
     /// Anchor center Y into current view's superview with a constant margin value.
     ///
     /// - Parameter withConstant: constant of the anchor constraint (default is 0).
@@ -97,25 +150,37 @@ extension UIView {
             centerYAnchor.constraint(equalTo: anchor, constant: constant).isActive = true
         }
     }
-
+    
     /// Anchor center X and Y into current view's superview
     func anchorCenterSuperview() {
         anchorCenterXToSuperview()
         anchorCenterYToSuperview()
     }
-
+    
+    /// Returns the `safeAreaLayoutGuide.trailingAnchor`, if available,
+    /// otherwhise, returns `trailingAnchor`
+    public var safeTrailingAnchor: NSLayoutXAxisAnchor {
+        safeAreaLayoutGuide.trailingAnchor
+    }
+    
+    /// Returns the `safeAreaLayoutGuide.leadingAnchor`, if available,
+    /// otherwhise, returns `leadingAnchor`
+    public var safeLeadingAnchor: NSLayoutXAxisAnchor {
+        safeAreaLayoutGuide.leadingAnchor
+    }
+    
     /// Returns the `safeAreaLayoutGuide.bottomAnchor`, if available,
     /// otherwhise, returns `bottomAnchor`
     var safeBottomAnchor: NSLayoutYAxisAnchor {
         return safeAreaLayoutGuide.bottomAnchor
     }
-
+    
     /// Returns the `safeAreaLayoutGuide.topAnchor`, if available,
     /// otherwhise, returns `topAnchor`
     var safeTopAnchor: NSLayoutYAxisAnchor {
         return safeAreaLayoutGuide.topAnchor
     }
-
+    
     /// Simplifies layouting with a constraint array
     /// - Parameter constraints: the constraints to apply to the view
     func layout(using constraints: [NSLayoutConstraint]) {
@@ -123,4 +188,3 @@ extension UIView {
         NSLayoutConstraint.activate(constraints)
     }
 }
-
