bc756b35d4ff0a4499ddb75f1071b7d16e74e5af
anacamargos <alcvlara@sga.pucminas.br>
2021-02-18 18:35:48 -0300

    Creating tests for configurator

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Tests/Sources/Extensions/MirrorExtensions.swift
@@ -0,0 +1,19 @@
+//
+//  MirrorExtensions.swift
+//  VeryUsefulToolstoRememberTests
+//
+//  Created by Ana Leticia Camargos on 18/02/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import Foundation
+
+extension Mirror {
+    public func firstChild<T>(of type: T.Type, in label: String? = nil) -> T? {
+        children.lazy.compactMap {
+            guard let value = $0.value as? T else { return nil }
+            guard let label = label else { return value }
+            return $0.label == label ? value : nil
+        }.first
+    }
+}
---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Tests/Tests/Scenes/Home/HomeConfiguratorTests.swift
@@ -0,0 +1,30 @@
+//
+//  HomeConfiguratorTests.swift
+//  VeryUsefulToolstoRememberTests
+//
+//  Created by Ana Leticia Camargos on 18/02/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import XCTest
+@testable import VeryUsefulToolstoRemember
+
+final class HomeConfiguratorTests: XCTestCase {
+
+    func test_configurator_shouldReturnCorrectlyConfiguredInstance() {
+            // Given
+            let sut = HomeConfigurator()
+            // When
+            let viewController = sut.resolveViewController()
+            
+            // Then
+            guard let router = Mirror(reflecting: viewController).firstChild(of: HomeRouter.self) else {
+                XCTFail("Could not find HomeRouter.")
+                return
+            }
+            
+            XCTAssertNotNil(router.viewController)
+            XCTAssertTrue(router.viewController is HomeViewController)
+        }
+
+}
