a63920b27f621689853a57b3329ba3d2ebba82c5
anacamargos <alcvlara@sga.pucminas.br>
2021-02-23 19:18:14 -0300

    Adding router dependency in view controller

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/AddNewTool/AddNewToolViewController.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/AddNewTool/AddNewToolViewController.swift
@@ -10,10 +10,46 @@ import UIKit
 
 final class AddNewToolViewController: UIViewController {
     
+    // MARK: - Dependencies
+    
+    private let router: AddNewToolRoutingLogic
+    
+    // MARK: - View Components
+    
+    weak var contentView: AddNewToolContentViewProtocol?
+    
+    // MARK: - Initializers
+    
+    init(
+        router: AddNewToolRoutingLogic
+    ) {
+        self.router = router
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // MARK: - View Controller Lifecycle
     
     override func loadView() {
         super.loadView()
-        view = AddNewToolContentView(onTappedCloseButtonClosure: {}, onTappedAddToolButtonClosure: { _ in })
+        view = AddNewToolContentView(
+            onTappedCloseButtonClosure: { [weak self] in self?.onTappedCloseButtonAction() },
+            onTappedAddToolButtonClosure: { [weak self] tool in self?.onTappedAddToolButtonAction(tool) }
+        )
+        contentView = view as? AddNewToolContentViewProtocol
+    }
+    
+    // MARK: - Private Methods
+    
+    private func onTappedCloseButtonAction() {
+        router.routeToPreviousScene()
+    }
+    
+    private func onTappedAddToolButtonAction(_ newTool: AddNewTool.Request) {
+        
     }
 }
