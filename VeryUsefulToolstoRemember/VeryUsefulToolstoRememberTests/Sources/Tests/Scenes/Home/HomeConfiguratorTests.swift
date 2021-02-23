2a73e1f7060020bddd13ea48ec3d5dbdf7866c61
anacamargos <alcvlara@sga.pucminas.br>
2021-02-23 19:19:19 -0300

    Updating test identation

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Scenes/Home/HomeConfiguratorTests.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Scenes/Home/HomeConfiguratorTests.swift
@@ -10,21 +10,21 @@ import XCTest
 @testable import VeryUsefulToolstoRemember
 
 final class HomeConfiguratorTests: XCTestCase {
-
+    
     func test_configurator_shouldReturnCorrectlyConfiguredInstance() {
-            // Given
-            let sut = HomeConfigurator()
-            // When
-            let viewController = sut.resolveViewController()
-            
-            // Then
-            guard let router = Mirror(reflecting: viewController).firstChild(of: HomeRouter.self) else {
-                XCTFail("Could not find HomeRouter.")
-                return
-            }
-            
-            XCTAssertNotNil(router.viewController)
-            XCTAssertTrue(router.viewController is HomeViewController)
+        // Given
+        let sut = HomeConfigurator()
+        // When
+        let viewController = sut.resolveViewController()
+        
+        // Then
+        guard let router = Mirror(reflecting: viewController).firstChild(of: HomeRouter.self) else {
+            XCTFail("Could not find HomeRouter.")
+            return
         }
-
+        
+        XCTAssertNotNil(router.viewController)
+        XCTAssertTrue(router.viewController is HomeViewController)
+    }
+    
 }
