90aa33336842d6e248a7f4aa09f9f64fb117c9f5
anacamargos <alcvlara@sga.pucminas.br>
2021-02-25 19:41:54 -0300

    Creating Interactor

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/Home/HomeInteractor.swift
@@ -0,0 +1,23 @@
+//
+//  HomeInteractor.swift
+//  VeryUsefulToolstoRemember
+//
+//  Created by Ana Leticia Camargos on 25/02/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import Foundation
+
+protocol HomeBusinessLogic {
+    
+}
+
+final class HomeInteractor {
+    
+}
+
+// MARK: - HomeBusinessLogic
+
+extension HomeInteractor: HomeBusinessLogic {
+    
+}
