05856f72f971ac32f76999ad124a3b48d4cf6578
anacamargos <alcvlara@sga.pucminas.br>
2021-03-10 19:05:41 -0300

    Creating presenter tests

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Scenes/RemoveTool/RemoveToolPresenterTests.swift
@@ -0,0 +1,48 @@
+//
+//  RemoveToolPresenterTests.swift
+//  VeryUsefulToolstoRememberTests
+//
+//  Created by Ana Leticia Camargos on 10/03/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import XCTest
+@testable import VeryUsefulToolstoRemember
+
+final class RemoveToolPresenterTests: XCTestCase {
+    
+    func test_presentToolName_shouldCallCorrectMethodInViewController() {
+        // Given
+        let viewControllerSpy = RemoveToolDisplayLogicSpy()
+        let sut = makeSUT(viewController: viewControllerSpy)
+        let toolName = "Notion"
+        
+        // When
+        sut.presentToolName(toolName)
+        
+        // Then
+        XCTAssertEqual(String(describing: viewControllerSpy.displayToolNamePassedToolNames), String(describing: [toolName]))
+    }
+
+    // MARK: - Test Helpers
+    
+    private func makeSUT(viewController: RemoveToolDisplayLogic = RemoveToolDisplayLogicDummy()) -> RemoveToolPresenter {
+        let sut = RemoveToolPresenter()
+        sut.viewController = viewController
+        return sut
+    }
+
+}
+
+final class RemoveToolDisplayLogicDummy: RemoveToolDisplayLogic {
+    func displayToolName(_ toolName: String) {}
+}
+
+final class RemoveToolDisplayLogicSpy: RemoveToolDisplayLogic {
+    
+    private(set) var displayToolNamePassedToolNames = [String]()
+    
+    func displayToolName(_ toolName: String) {
+        displayToolNamePassedToolNames.append(toolName)
+    }
+}
