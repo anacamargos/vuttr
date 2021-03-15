f255c6aea11b747e979ecfaf643824f02b80c61f
anacamargos <alcvlara@sga.pucminas.br>
2021-03-15 20:56:59 -0300

    Creating empty view

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/UI/Views/EmptyView.swift
@@ -0,0 +1,47 @@
+//
+//  EmptyView.swift
+//  VeryUsefulToolstoRemember
+//
+//  Created by Ana Leticia Camargos on 15/03/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import UIKit
+
+final class EmptyView: CodedView {
+    
+    // MARK: - View Components
+    
+    private let emptyLabel: UILabel = {
+        let label = UILabel()
+        label.font = .themeFont(for: .body, weight: .semibold)
+        label.textColor = .ink
+        label.numberOfLines = .zero
+        return label
+    }()
+    
+    // MARK: - Override Methods
+    
+    override func addSubviews() {
+        addSubview(emptyLabel)
+    }
+    
+    override func constrainSubviews() {
+        emptyLabel.anchor(
+            top: topAnchor,
+            leading: leadingAnchor,
+            bottom: bottomAnchor,
+            trailing: trailingAnchor,
+            topConstant: Metrics.Spacing.xLarge,
+            leadingConstant: Metrics.Spacing.small,
+            bottomConstant: Metrics.Spacing.xLarge,
+            trailingConstant: Metrics.Spacing.small
+        )
+    }
+    
+    // MARK: - Public Methods
+    
+    func setupLabelText(_ text: String) {
+        emptyLabel.text = text
+    }
+}
