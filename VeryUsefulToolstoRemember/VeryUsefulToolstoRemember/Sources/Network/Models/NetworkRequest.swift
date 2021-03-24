db0cc41d738c39a41fe7bf3a9d362c4d03f78384
anacamargos <alcvlara@sga.pucminas.br>
2021-03-24 18:54:27 -0300

    Creating network request

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Network/Models/NetworkRequest.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkRequest.swift
+//  VeryUsefulToolstoRemember
+//
+//  Created by Ana Leticia Camargos on 24/03/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkRequest {
+    let url: URL
+}
