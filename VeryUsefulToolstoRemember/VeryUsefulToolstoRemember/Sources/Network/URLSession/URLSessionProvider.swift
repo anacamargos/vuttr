5c2f31051017cf6b354f0ce52b9ec4fe77ce5ccf
anacamargos <alcvlara@sga.pucminas.br>
2021-03-25 18:17:18 -0300

    Creating URLSessionProvider

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Network/URLSession/URLSessionProvider.swift
@@ -0,0 +1,15 @@
+//
+//  URLSessionProvider.swift
+//  VeryUsefulToolstoRemember
+//
+//  Created by Ana Leticia Camargos on 25/03/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import Foundation
+
+protocol URLSessionProvider {
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+extension URLSession: URLSessionProvider {}
