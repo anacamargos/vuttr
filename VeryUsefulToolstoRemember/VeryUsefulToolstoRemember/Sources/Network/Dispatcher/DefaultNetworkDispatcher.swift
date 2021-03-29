dba5cefb1ce2944c2792e3120d792a3d03e3e4ee
anacamargos <alcvlara@sga.pucminas.br>
2021-03-29 17:26:37 -0300

    Updating dispatcher to decode network response

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Network/Dispatcher/DefaultNetworkDispatcher.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Network/Dispatcher/DefaultNetworkDispatcher.swift
@@ -13,11 +13,16 @@ final class DefaultNetworkDispatcher: NetworkDispatcher {
     // MARK: - Dependencies
     
     private let httpClient: HTTPClient
+    private let responseDecoder: NetworkResponseDecoder
     
     // MARK: - Initializers
     
-    init(httpClient: HTTPClient) {
+    init(
+        httpClient: HTTPClient,
+        responseDecoder: NetworkResponseDecoder
+    ) {
         self.httpClient = httpClient
+        self.responseDecoder = responseDecoder
     }
     
     // MARK: - NetworkDispatcher
@@ -29,8 +34,8 @@ final class DefaultNetworkDispatcher: NetworkDispatcher {
     }
     
     func requestCodable<T>(ofType type: T.Type, for request: NetworkRequest, then handle: @escaping (Result<T?, NetworkError>) -> Void) where T : Decodable, T : Encodable {
-        dispatch(request) { result in
-            
+        dispatch(request) { [weak self] result in
+            self?.responseDecoder.decodeDataRequestResult(result, ofType: type, then: handle)
         }
     }
 }
