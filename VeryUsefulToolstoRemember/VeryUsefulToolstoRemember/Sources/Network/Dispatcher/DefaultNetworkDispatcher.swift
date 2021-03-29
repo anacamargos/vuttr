4d1a3075290761869d5f0ab1f2e0deeb3e32d55c
anacamargos <alcvlara@sga.pucminas.br>
2021-03-29 17:12:54 -0300

    Adding http client dependency in network dispatcher

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Network/Dispatcher/DefaultNetworkDispatcher.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Network/Dispatcher/DefaultNetworkDispatcher.swift
@@ -10,13 +10,27 @@ import Foundation
 
 final class DefaultNetworkDispatcher: NetworkDispatcher {
     
+    // MARK: - Dependencies
+    
+    private let httpClient: HTTPClient
+    
+    // MARK: - Initializers
+    
+    init(httpClient: HTTPClient) {
+        self.httpClient = httpClient
+    }
+    
     // MARK: - NetworkDispatcher
     
     func dispatch(_ request: NetworkRequest, then handle: @escaping (Result<NetworkResponse, NetworkError>) -> Void) {
-        
+        httpClient.get(from: request.url) { result in
+            handle(result)
+        }
     }
     
     func requestCodable<T>(ofType type: T.Type, for request: NetworkRequest, then handle: @escaping (Result<T?, NetworkError>) -> Void) where T : Decodable, T : Encodable {
-        
+        dispatch(request) { result in
+            
+        }
     }
 }
