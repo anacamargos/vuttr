05e2b6211858c666b330b8a74d56b760d55d14d6
anacamargos <alcvlara@sga.pucminas.br>
2021-04-05 11:40:33 -0300

    Creating DefaultRequestBuilder class

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Network/Builders/DefaultRequestBuilder.swift
@@ -0,0 +1,28 @@
+//
+//  DefaultRequestBuilder.swift
+//  VeryUsefulToolstoRemember
+//
+//  Created by Ana Leticia Camargos on 05/04/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import Foundation
+
+final class DefaultRequestBuilder: NetworkRequestBuilder {
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    init(request: NetworkRequest) {
+        
+    }
+    
+    // MARK: - NetworkRequestBuilder
+    
+    func build() throws -> URLRequest {
+        
+    }
+    
+    
+}
