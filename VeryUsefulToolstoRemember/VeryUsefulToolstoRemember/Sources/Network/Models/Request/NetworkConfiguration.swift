f2bc3b1fdc932c7b3bd55d36cb75d719e56cad24
anacamargos <alcvlara@sga.pucminas.br>
2021-04-05 11:49:00 -0300

    Creating NetworkConfiguration

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Network/Models/Request/NetworkConfiguration.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkConfiguration.swift
+//  VeryUsefulToolstoRemember
+//
+//  Created by Ana Leticia Camargos on 05/04/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkConfiguration {
+    let urlProvider: URLProvider
+}
