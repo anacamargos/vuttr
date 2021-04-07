9f69e42b28d25e21d499426b40ca2adcfd0668d9
anacamargos <alcvlara@sga.pucminas.br>
2021-04-07 18:14:17 -0300

    Creating DefaultRequestBuilderTests file

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Network/Builders/DefaultRequestBuilderTests.swift
@@ -0,0 +1,14 @@
+//
+//  DefaultRequestBuilderTests.swift
+//  VeryUsefulToolstoRememberTests
+//
+//  Created by Ana Leticia Camargos on 07/04/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import XCTest
+@testable import VeryUsefulToolstoRemember
+
+final class DefaultRequestBuilderTests: XCTestCase {
+
+}
