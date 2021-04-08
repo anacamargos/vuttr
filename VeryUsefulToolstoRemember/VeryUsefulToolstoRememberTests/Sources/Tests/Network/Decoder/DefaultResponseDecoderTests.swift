d7434b68a179456a0e54aaebca5d1facc36ee549
anacamargos <alcvlara@sga.pucminas.br>
2021-04-08 15:15:29 -0300

    Creating DefaultResponseDecoderTests file

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Network/Decoder/DefaultResponseDecoderTests.swift
@@ -0,0 +1,16 @@
+//
+//  DefaultResponseDecoderTests.swift
+//  VeryUsefulToolstoRememberTests
+//
+//  Created by Ana Leticia Camargos on 08/04/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import XCTest
+@testable import VeryUsefulToolstoRemember
+
+final class DefaultResponseDecoderTests: XCTestCase {
+
+    
+
+}
