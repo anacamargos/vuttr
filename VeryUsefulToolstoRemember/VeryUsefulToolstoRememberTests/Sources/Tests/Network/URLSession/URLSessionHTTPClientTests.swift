27d2f28a6dc4dbd314fccf1586efc638f6508421
anacamargos <alcvlara@sga.pucminas.br>
2021-04-09 12:01:38 -0300

    Creating URLSessionHTTPClientTests file

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Network/URLSession/URLSessionHTTPClientTests.swift
@@ -0,0 +1,16 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  VeryUsefulToolstoRememberTests
+//
+//  Created by Ana Leticia Camargos on 09/04/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import XCTest
+@testable import VeryUsefulToolstoRemember
+
+final class URLSessionHTTPClientTests: XCTestCase {
+
+    
+
+}
