70bea7bbe202d2f1a7c76bd7e5dfe42c8351b9b9
anacamargos <alcvlara@sga.pucminas.br>
2021-04-09 12:24:20 -0300

    Creating test for url session

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Network/URLSession/URLSessionHTTPClientTests.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Network/URLSession/URLSessionHTTPClientTests.swift
@@ -10,7 +10,72 @@ import XCTest
 @testable import VeryUsefulToolstoRemember
 
 final class URLSessionHTTPClientTests: XCTestCase {
-
     
+    func test_get_whenRequestIsInvalid_shouldReturnInvalidBaseURLError() {
+        // Given
+        let sut = makeSUT()
+        let httpRequest = HTTPRequest(baseURL: .string(""), method: .get)
+        let expectedError = NetworkError(.internal(.invalidBaseURL))
+        
+        // When
+        guard let receivedError = resultErrorFor(networkRequest: httpRequest, sut: sut) else {
+            XCTFail("Could not find receivedError")
+            return
+        }
+        
+        // Then
+        XCTAssertEqual(String(describing: receivedError), String(describing: expectedError))
+    }
 
+    // MARK: - Test Helpers
+    
+    private func makeSUT(
+        session: URLSessionProvider = URLSessionDummy(),
+        configuration: NetworkConfiguration = NetworkConfiguration(urlProvider: URLProviderDummy()),
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> URLSessionHTTPClient {
+        let sut = URLSessionHTTPClient(session: session, configuration: configuration)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    private func resultFor(
+        request: NetworkRequest,
+        sut: URLSessionHTTPClient,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> Result<NetworkResponse, NetworkError> {
+         
+        var receivedResult: Result<NetworkResponse, NetworkError>!
+        let exp = expectation(description: "Wait for completion")
+        sut.get(from: request) { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+        return receivedResult
+    }
+    
+    private func resultErrorFor(
+        networkRequest: NetworkRequest,
+        sut: URLSessionHTTPClient,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> Error? {
+        let result = resultFor(request: networkRequest, sut: sut)
+        switch result {
+        case let .failure(error):
+            return error
+        default:
+            XCTFail("Expected failure, got \(result) instead", file: file, line: line)
+            return nil
+        }
+    }
+}
+
+final class URLSessionDummy: URLSessionProvider {
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return .init()
+    }
 }
