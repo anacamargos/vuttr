62d98bbce85f2c5acc678434bc82d25208368d04
anacamargos <alcvlara@sga.pucminas.br>
2021-04-12 10:12:17 -0300

    Creating DefaultNetworkDispatcherTests file

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Network/Dispatcher/DefaultNetworkDispatcherTests.swift
@@ -0,0 +1,15 @@
+//
+//  DefaultNetworkDispatcherTests.swift
+//  VeryUsefulToolstoRememberTests
+//
+//  Created by Ana Leticia Camargos on 12/04/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import XCTest
+@testable import VeryUsefulToolstoRemember
+
+final class DefaultNetworkDispatcherTests: XCTestCase {
+
+
+}
