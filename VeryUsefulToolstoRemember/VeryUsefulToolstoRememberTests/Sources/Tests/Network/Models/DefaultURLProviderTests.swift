80fcab0f0c4ad38d71ca5a62829af83e96caf209
anacamargos <alcvlara@sga.pucminas.br>
2021-04-13 10:48:04 -0300

    Creating DefaultURLProviderTests file

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Network/Models/DefaultURLProviderTests.swift
@@ -0,0 +1,29 @@
+//
+//  DefaultURLProviderTests.swift
+//  VeryUsefulToolstoRememberTests
+//
+//  Created by Ana Leticia Camargos on 13/04/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import XCTest
+@testable import VeryUsefulToolstoRemember
+
+final class DefaultURLProviderTests: XCTestCase {
+    
+    func test_() {
+        // Given
+        let sut = makeSUT()
+        
+        // When
+        
+        // Then
+    }
+    
+    // MARK: - Test Helpers
+    
+    private func makeSUT() -> DefaultURLProvider {
+        .init()
+    }
+
+}
