51e4bce3df8030252258a7f94c8b4a05bd275af1
anacamargos <alcvlara@sga.pucminas.br>
2021-04-14 15:04:43 -0300

    Creating tools request

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Services/ToolsRequest.swift
@@ -0,0 +1,38 @@
+//
+//  ToolsRequest.swift
+//  VeryUsefulToolstoRemember
+//
+//  Created by Ana Leticia Camargos on 14/04/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import Foundation
+
+enum ToolsRequest: NetworkRequest {
+    
+    case getTools
+    case createNewTool
+    case deleteTool(id: UInt)
+    
+    var baseURL: BaseURL { .serviceGroup(.vuttr) }
+    
+    var path: String? {
+        switch self {
+        case .getTools, .createNewTool:
+            return "/tools"
+        case let .deleteTool(id):
+            return "/tools/\(id)"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .getTools:
+            return .get
+        case .createNewTool:
+            return .post
+        case .deleteTool:
+            return .delete
+        }
+    }
+}
