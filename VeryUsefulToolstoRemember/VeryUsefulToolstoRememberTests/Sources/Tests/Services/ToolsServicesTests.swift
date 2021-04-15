31b158320e57745b0875cd845cead6db955630ab
anacamargos <alcvlara@sga.pucminas.br>
2021-04-15 17:53:14 -0300

    Creating dummy and stub for service

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Services/ToolsServicesTests.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Services/ToolsServicesTests.swift
@@ -11,5 +11,44 @@ import XCTest
 
 final class ToolsServicesTests: XCTestCase {
 
+    // MARK: - Test Helpers
+    
+    private func makeSUT(networkDispatcher: NetworkDispatcher) -> ToolsServices {
+        let sut = ToolsServices(networkDispatcher: networkDispatcher)
+        return sut
+    }
+    
+}
 
+final class NetworkDispatcherDummy: NetworkDispatcher {
+    func dispatch(_ request: NetworkRequest, then handle: @escaping (Result<NetworkResponse, NetworkError>) -> Void) {}
+    func requestCodable<T>(ofType type: T.Type, for request: NetworkRequest, then handle: @escaping (Result<T?, NetworkError>) -> Void) where T : Decodable, T : Encodable {}
+}
+
+final class NetworkDispatcherMock<T: Codable>: NetworkDispatcher {
+    
+    var dispatchResultToBeReturned: Result<NetworkResponse, NetworkError> = .success(.init(status: .http(200), data: nil))
+    private(set) var dispatchPassedRequests = [NetworkRequest]()
+    
+    func dispatch(_ request: NetworkRequest, then handle: @escaping (Result<NetworkResponse, NetworkError>) -> Void) {
+        dispatchPassedRequests.append(request)
+        handle(dispatchResultToBeReturned)
+    }
+    
+    var requestCodableResultToBeReturned: Result<T?, NetworkError> = .success(nil)
+    private(set) var requestCodablePassedRequests = [NetworkRequest]()
+    
+    func requestCodable<T>(ofType type: T.Type, for request: NetworkRequest, then handle: @escaping (Result<T?, NetworkError>) -> Void) where T : Decodable, T : Encodable {
+        requestCodablePassedRequests.append(request)
+        switch requestCodableResultToBeReturned {
+        case let .failure(error):
+            handle(.failure(error))
+        case let .success(anyValue):
+            var valueToReturn: T?
+            if let anyValue = anyValue, let castedValue = anyValue as? T {
+                valueToReturn = castedValue
+            }
+            handle(.success(valueToReturn))
+        }
+    }
 }
