689cb9137efcd36689af5d725854fbfc86b8e4b5
anacamargos <alcvlara@sga.pucminas.br>
2021-04-15 17:46:41 -0300

    Creating ToolsServicesTests file

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Services/ToolsServicesTests.swift
@@ -0,0 +1,15 @@
+//
+//  ToolsServicesTests.swift
+//  VeryUsefulToolstoRememberTests
+//
+//  Created by Ana Leticia Camargos on 15/04/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import XCTest
+@testable import VeryUsefulToolstoRemember
+
+final class ToolsServicesTests: XCTestCase {
+
+
+}
