16b94282bf7e6e6df8ddf0309110d3e2aadff2f3
anacamargos <alcvlara@sga.pucminas.br>
2021-04-15 18:06:12 -0300

    Creating tests for service

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Services/ToolsServicesTests.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Services/ToolsServicesTests.swift
@@ -10,12 +10,49 @@ import XCTest
 @testable import VeryUsefulToolstoRemember
 
 final class ToolsServicesTests: XCTestCase {
+    
+    func test_getAllTools_whenRequestFails_shouldReturnCorrectError() {
+        // Given
+        let dispatcherMock = NetworkDispatcherMock<[ToolResponseEntity]>()
+        let sut = makeSUT(networkDispatcher: dispatcherMock)
+        let errorMock = NetworkError(.internal(.noInternetConnection))
+        dispatcherMock.requestCodableResultToBeReturned = .failure(errorMock)
+        let expectedError = ToolsServiceError.genericError
+        
+        // When
+        expect(sut, toCompleteWith: .failure(expectedError))
+        
+        // Then
+        XCTAssertEqual(String(describing: dispatcherMock.requestCodablePassedRequests), String(describing: [ToolsRequest.getTools]))
+    }
 
     // MARK: - Test Helpers
     
     private func makeSUT(networkDispatcher: NetworkDispatcher) -> ToolsServices {
         let sut = ToolsServices(networkDispatcher: networkDispatcher)
         return sut
+    }
+    
+    private func expect(
+        _ sut: ToolsServices,
+        toCompleteWith expectedResult: Result<[ToolResponseEntity], ToolsServiceError>,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        let exp = expectation(description: "Wait for completion")
+        sut.getAllTools { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedItems), .success(expectedItems)):
+                XCTAssertEqual(String(describing: receivedItems), String(describing: expectedItems), file: file, line: line)
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+            default:
+                XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
     }
     
 }
