772a1ed670e073dd74060cdef7c17ee10e04cca6
anacamargos <alcvlara@sga.pucminas.br>
2021-04-16 16:49:10 -0300

    Moving entity extension to its own file

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Extensions/ToolResponseEntityExtensions.swift
@@ -0,0 +1,16 @@
+//
+//  ToolResponseEntityExtensions.swift
+//  VeryUsefulToolstoRememberTests
+//
+//  Created by Ana Leticia Camargos on 16/04/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import Foundation
+@testable import VeryUsefulToolstoRemember
+
+extension ToolResponseEntity {
+    static var mock: ToolResponseEntity {
+        .init(id: 0, title: "Notion", link: "https://notion.so", description: "All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized.", tags: ["organization", "planning", "collaboration", "writing", "calendar"])
+    }
+}
--- VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Services/ToolsServicesTests.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Services/ToolsServicesTests.swift
@@ -84,9 +84,3 @@ final class ToolsServicesTests: XCTestCase {
     }
     
 }
-
-extension ToolResponseEntity {
-    static var mock: ToolResponseEntity {
-        .init(id: 0, title: "Notion", link: "https://notion.so", description: "All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized.", tags: ["organization", "planning", "collaboration", "writing", "calendar"])
-    }
-}
