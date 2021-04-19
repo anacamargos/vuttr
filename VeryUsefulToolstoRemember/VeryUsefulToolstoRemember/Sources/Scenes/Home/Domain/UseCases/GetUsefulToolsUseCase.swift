6e07cd05dd8f6847479ea1c74d28d91413c44a02
anacamargos <alcvlara@sga.pucminas.br>
2021-04-19 14:32:12 -0300

    Updating use case to use service and get real data

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/Home/Domain/UseCases/GetUsefulToolsUseCase.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/Home/Domain/UseCases/GetUsefulToolsUseCase.swift
@@ -14,14 +14,38 @@ protocol GetUsefulToolsUseCaseProvider {
 
 final class GetUsefulToolsUseCase: GetUsefulToolsUseCaseProvider {
     
+    // MARK: - Dependencies
+    
+    private let service: ToolsServicesProvider
+    
+    // MARK: - Initializer
+    
+    init(service: ToolsServicesProvider) {
+        self.service = service
+    }
+    
     // MARK: - GetUsefulToolsUseCaseProvider
     
     func execute(then handle: @escaping (Result<[GetUsefulToolsUseCaseModels.Tool], GetUsefulToolsUseCaseError>) -> Void) {
-        let notion = GetUsefulToolsUseCaseModels.Tool(id: 1, title: "Notion", link: "https://notion.so", description: "All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized. ", tags: ["organization", "planning", "collaboration", "writing", "calendar"])
-        let jsonServer = GetUsefulToolsUseCaseModels.Tool(id: 2, title: "json-server", link: "https://github.com/typicode/json-server", description: "Fake REST API based on a json schema. Useful for mocking and creating APIs for front-end devs to consume in coding challenges.", tags: ["api", "json", "schema", "node", "github", "rest"])
-        let fastiky = GetUsefulToolsUseCaseModels.Tool(id: 3, title: "fastify", link: "https://www.fastify.io/", description: "Extremely fast and simple, low-overhead web framework for NodeJS. Supports HTTP2.", tags: ["web", "framework", "node", "http2", "https", "localhost"])
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
-            handle(.success([notion, jsonServer, fastiky]))
+        service.getAllTools { [weak self] result in
+            switch result {
+            case let .success(data):
+                self?.handleSuccess(input: data, then: handle)
+            case .failure:
+                handle(.failure(.genericError))
+            }
         }
+    }
+    
+    // MARK: - Private Methods
+    
+    private func handleSuccess(
+        input: [ToolResponseEntity],
+        then handle: @escaping (Result<[GetUsefulToolsUseCaseModels.Tool], GetUsefulToolsUseCaseError>) -> Void
+    ) {
+        let domainTools = input.map {
+            GetUsefulToolsUseCaseModels.Tool(id: $0.id, title: $0.title, link: $0.link, description: $0.description, tags: $0.tags)
+        }
+        handle(.success(domainTools))
     }
 }
