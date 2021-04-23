6169095e566d2d18ea3226f422cf01ee2a0d88dd
anacamargos <alcvlara@sga.pucminas.br>
2021-04-23 19:31:37 -0300

    Creating DeleteToolUseCase

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/RemoveTool/Domain/UseCases/DeleteToolUseCase.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/RemoveTool/Domain/UseCases/DeleteToolUseCase.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+enum DeleteToolUseCaseError: Error {
+    case genericError
+}
+
+protocol DeleteToolUseCaseProvider {
+    func execute(toolId: UInt, then handle: @escaping (Result<NoEntity, DeleteToolUseCaseError>) -> Void)
+}
+
+final class DeleteToolUseCase: DeleteToolUseCaseProvider {
+    
+    // MARK: - Dependencies
+
+    private let service: ToolsServicesProvider
+
+    // MARK: - Initializer
+
+    init(service: ToolsServicesProvider) {
+        self.service = service
+    }
+    
+    // MARK: - DeleteToolUseCaseProvider
+    
+    func execute(toolId: UInt, then handle: @escaping (Result<NoEntity, DeleteToolUseCaseError>) -> Void) {
+        service.deleteTool(id: toolId) { result in
+            switch result {
+            case .success:
+                handle(.success(.init()))
+            case .failure:
+                handle(.failure(.genericError))
+            }
+        }
+    }
+}
