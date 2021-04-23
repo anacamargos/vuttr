17eb036ad40e98cc96c05a0f49291eb2c2e19dac
anacamargos <alcvlara@sga.pucminas.br>
2021-04-23 19:31:25 -0300

    Creating new file for use case

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/RemoveTool/Domain/UseCases/DeleteToolUseCase.swift
@@ -0,0 +1,9 @@
+//
+//  DeleteToolUseCase.swift
+//  VeryUsefulToolstoRemember
+//
+//  Created by Ana Leticia Camargos on 23/04/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import Foundation
