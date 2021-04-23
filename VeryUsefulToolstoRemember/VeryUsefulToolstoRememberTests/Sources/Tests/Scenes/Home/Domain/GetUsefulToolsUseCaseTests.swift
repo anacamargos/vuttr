f1989e8e4a923dd269d44586b4ad209dddd6a4e7
anacamargos <alcvlara@sga.pucminas.br>
2021-04-23 19:36:17 -0300

    Creating test helper

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Scenes/Home/Domain/GetUsefulToolsUseCaseTests.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Scenes/Home/Domain/GetUsefulToolsUseCaseTests.swift
@@ -11,4 +11,32 @@ import XCTest
 
 final class GetUsefulToolsUseCaseTests {
     
+    // MARK: - Test Helpers
+    
+    private func makeSUT(
+        service: ToolsServicesProvider = ToolsServicesDummy()
+    ) -> GetUsefulToolsUseCase {
+        let sut = GetUsefulToolsUseCase(service: service)
+        return sut
+    }
+}
+
+final class ToolsServicesDummy: ToolsServicesProvider {
+    func getAllTools(then handle: @escaping (Result<[ToolResponseEntity], ToolsServiceError>) -> Void) {}
+    func deleteTool(id: UInt, then handle: @escaping (Result<NoEntity, ToolsServiceError>) -> Void) {}
+}
+
+final class ToolsServicesStub: ToolsServicesProvider {
+    
+    var getAllToolsResultToBeReturned: Result<[ToolResponseEntity], ToolsServiceError> = .success([.mock])
+    
+    func getAllTools(then handle: @escaping (Result<[ToolResponseEntity], ToolsServiceError>) -> Void) {
+        handle(getAllToolsResultToBeReturned)
+    }
+    
+    var deleteToolResultToBeReturned: Result<NoEntity, ToolsServiceError> = .success(.init())
+    
+    func deleteTool(id: UInt, then handle: @escaping (Result<NoEntity, ToolsServiceError>) -> Void) {
+        handle(deleteToolResultToBeReturned)
+    }
 }
