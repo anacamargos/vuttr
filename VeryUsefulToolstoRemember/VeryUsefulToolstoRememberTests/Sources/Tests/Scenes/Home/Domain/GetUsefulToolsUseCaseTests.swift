6f8de80d62b64d324849bb097fc79a8aba81ebf2
anacamargos <alcvlara@sga.pucminas.br>
2021-04-23 19:33:34 -0300

    Creating GetUsefulToolsUseCaseTests file

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Scenes/Home/Domain/GetUsefulToolsUseCaseTests.swift
@@ -0,0 +1,14 @@
+//
+//  GetUsefulToolsUseCaseTests.swift
+//  VeryUsefulToolstoRememberTests
+//
+//  Created by Ana Leticia Camargos on 23/04/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import XCTest
+@testable import VeryUsefulToolstoRemember
+
+final class GetUsefulToolsUseCaseTests {
+    
+}
