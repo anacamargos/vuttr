b1aed38ec18567e773f580481856441718c2d846
anacamargos <alcvlara@sga.pucminas.br>
2021-05-04 16:54:52 -0300

    Creating new use case

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/AddNewTool/Domain/CreateNewToolUseCase.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/AddNewTool/Domain/CreateNewToolUseCase.swift
@@ -8,6 +8,46 @@
 
 import Foundation
 
-final class CreateNewToolUseCase {
-    
+enum CreateNewToolUseCaseError: Error {
+    case genericError
+}
+
+protocol CreateNewToolUseCaseProvider {
+    func execute(parameters: CreateNewToolParameters, then handle: @escaping (Result<GetUsefulToolsUseCaseModels.Tool, CreateNewToolUseCaseError>) -> Void)
+}
+
+final class CreateNewToolUseCase: CreateNewToolUseCaseProvider {
+
+    // MARK: - Dependencies
+
+    private let service: ToolsServicesProvider
+
+    // MARK: - Initializer
+
+    init(service: ToolsServicesProvider) {
+        self.service = service
+    }
+
+    // MARK: - CreateNewToolUseCaseProvider
+
+    func execute(parameters: CreateNewToolParameters, then handle: @escaping (Result<GetUsefulToolsUseCaseModels.Tool, CreateNewToolUseCaseError>) -> Void) {
+        service.createNewTool(parameters: parameters) { [weak self] result in
+            switch result {
+            case let .success(data):
+                self?.handleSuccess(input: data, then: handle)
+            case .failure:
+                handle(.failure(.genericError))
+            }
+        }
+    }
+
+    // MARK: - Private Methods
+
+    private func handleSuccess(
+        input: ToolResponseEntity,
+        then handle: @escaping (Result<GetUsefulToolsUseCaseModels.Tool, CreateNewToolUseCaseError>) -> Void
+    ) {
+        let domainTools = GetUsefulToolsUseCaseModels.Tool(id: input.id, title: input.title, link: input.link, description: input.description, tags: input.tags)
+        handle(.success(domainTools))
+    }
 }
