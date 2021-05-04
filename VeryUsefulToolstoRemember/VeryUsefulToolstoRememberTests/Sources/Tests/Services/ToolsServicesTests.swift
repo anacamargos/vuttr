99679c01ae48e0ac3a6b76630ec1dc9014fb5e66
anacamargos <alcvlara@sga.pucminas.br>
2021-05-04 16:49:25 -0300

    Creating new tests

--- VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Services/ToolsServicesTests.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Services/ToolsServicesTests.swift
@@ -83,6 +83,49 @@ final class ToolsServicesTests: XCTestCase {
         XCTAssertEqual(String(describing: dispatcherMock.dispatchPassedRequests), String(describing: [ToolsRequest.deleteTool(id: .zero)]))
     }
 
+    func test_createNewTool_whenRequestFails_shouldReturnCorrectError() {
+        // Given
+        let dispatcherMock = NetworkDispatcherMock<ToolResponseEntity>()
+        let sut = makeSUT(networkDispatcher: dispatcherMock)
+        let errorMock = NetworkError(.internal(.noInternetConnection))
+        dispatcherMock.requestCodableResultToBeReturned = .failure(errorMock)
+        let expectedError = ToolsServiceError.genericError
+
+        // When
+        createNewToolExpect(sut, toCompleteWith: .failure(expectedError))
+
+        // Then
+        XCTAssertEqual(String(describing: dispatcherMock.requestCodablePassedRequests), String(describing: [ToolsRequest.createNewTool(.mock)]))
+    }
+
+    func test_createNewTool_whenRequestSucceeds_butResponseIsInvalid_shouldReturnCorrectError() {
+        // Given
+        let dispatcherMock = NetworkDispatcherMock<ToolResponseEntity>()
+        let sut = makeSUT(networkDispatcher: dispatcherMock)
+        dispatcherMock.requestCodableResultToBeReturned = .success(nil)
+        let expectedError = ToolsServiceError.responseParse
+
+        // When
+        createNewToolExpect(sut, toCompleteWith: .failure(expectedError))
+
+        // Then
+        XCTAssertEqual(String(describing: dispatcherMock.requestCodablePassedRequests), String(describing: [ToolsRequest.createNewTool(.mock)]))
+    }
+
+    func test_createNewTool_whenRequestSucceedsWithValidResponse_shouldReturnCorrectData() {
+        // Given
+        let dispatcherMock = NetworkDispatcherMock<ToolResponseEntity>()
+        let sut = makeSUT(networkDispatcher: dispatcherMock)
+        dispatcherMock.requestCodableResultToBeReturned = .success(.mock)
+        let expectedResponse = ToolResponseEntity.mock
+
+        // When
+        createNewToolExpect(sut, toCompleteWith: .success(expectedResponse))
+
+        // Then
+        XCTAssertEqual(String(describing: dispatcherMock.requestCodablePassedRequests), String(describing: [ToolsRequest.createNewTool(.mock)]))
+    }
+
     // MARK: - Test Helpers
 
     private func makeSUT(networkDispatcher: NetworkDispatcher) -> ToolsServices {
@@ -108,7 +151,6 @@ final class ToolsServicesTests: XCTestCase {
             }
             exp.fulfill()
         }
-
         wait(for: [exp], timeout: 1.0)
     }
 
@@ -129,9 +171,40 @@ final class ToolsServicesTests: XCTestCase {
                 XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
             }
             exp.fulfill()
-
         }
         wait(for: [exp], timeout: 1.0)
     }
 
+    private func createNewToolExpect(
+        _ sut: ToolsServices,
+        toCompleteWith expectedResult: Result<ToolResponseEntity, ToolsServiceError>,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        let exp = expectation(description: "Wait for completion")
+        sut.createNewTool(parameters: .mock) { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedItems), .success(expectedItems)):
+                XCTAssertEqual(String(describing: receivedItems), String(describing: expectedItems), file: file, line: line)
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+            default:
+                XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+
+}
+
+extension CreateNewToolParameters {
+    static var mock: CreateNewToolParameters {
+        .init(
+            title: "Notion",
+            link: "https://notion.so",
+            description: "All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized.",
+            tags: ["organization", "planning", "collaboration", "writing"]
+        )
+    }
 }
