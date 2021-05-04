9a48d28df289a13902cd8a0ec8d4a827e3900914
anacamargos <alcvlara@sga.pucminas.br>
2021-05-04 16:50:46 -0300

    Creating CreateNewToolUseCase file

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/AddNewTool/Domain/CreateNewToolUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  CreateNewToolUseCase.swift
+//  VeryUsefulToolstoRemember
+//
+//  Created by Ana Leticia Camargos on 04/05/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import Foundation
+
+final class CreateNewToolUseCase {
+    
+}
