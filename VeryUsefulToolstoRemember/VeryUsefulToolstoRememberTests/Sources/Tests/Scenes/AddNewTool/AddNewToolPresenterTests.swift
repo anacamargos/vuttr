89b62ac6b227525352e04a9503665d1be284aa7c
anacamargos <alcvlara@sga.pucminas.br>
2021-05-10 17:22:10 -0300

    Creating presenter tests

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRememberTests/Sources/Tests/Scenes/AddNewTool/AddNewToolPresenterTests.swift
@@ -0,0 +1,76 @@
+//
+//  AddNewToolPresenterTests.swift
+//  VeryUsefulToolstoRememberTests
+//
+//  Created by Ana Leticia Camargos on 10/05/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import XCTest
+@testable import VeryUsefulToolstoRemember
+
+final class AddNewToolPresenterTests: XCTestCase {
+
+    func test_presentRemoveToolResponse_whenResponseIsSuccess_shouldCallCorrectMethodInViewController() {
+        // Given
+        let viewControllerSpy = AddNewToolDisplayLogicSpy()
+        let sut = makeSUT(viewController: viewControllerSpy)
+        let expectedViewState = AddNewTool.ViewState.success
+
+        // When
+        sut.presentToolsResponse(.success)
+
+        // Then
+        XCTAssertEqual(String(describing: viewControllerSpy.displayAddToolViewStatePassedViewStates), String(describing: [expectedViewState]))
+    }
+
+    func test_presentRemoveToolResponse_whenResponseIsLoading_shouldCallCorrectMethodInViewController() {
+        // Given
+        let viewControllerSpy = AddNewToolDisplayLogicSpy()
+        let sut = makeSUT(viewController: viewControllerSpy)
+        let expectedViewState = AddNewTool.ViewState.loading
+
+        // When
+        sut.presentToolsResponse(.loading)
+
+        // Then
+        XCTAssertEqual(String(describing: viewControllerSpy.displayAddToolViewStatePassedViewStates), String(describing: [expectedViewState]))
+    }
+
+    func test_presentRemoveToolResponse_whenResponseIsError_shouldCallCorrectMethodInViewController() {
+        // Given
+        let viewControllerSpy = AddNewToolDisplayLogicSpy()
+        let sut = makeSUT(viewController: viewControllerSpy)
+        let expectedViewState = AddNewTool.ViewState.error
+
+        // When
+        sut.presentToolsResponse(.error)
+
+        // Then
+        XCTAssertEqual(String(describing: viewControllerSpy.displayAddToolViewStatePassedViewStates), String(describing: [expectedViewState]))
+    }
+
+    // MARK: - Test Helpers
+
+    private func makeSUT(
+        viewController: AddNewToolDisplayLogic = AddNewToolDisplayLogicDummy()
+    ) -> AddNewToolPresenter {
+        let sut = AddNewToolPresenter()
+        sut.viewController = viewController
+        return sut
+    }
+
+}
+
+final class AddNewToolDisplayLogicDummy: AddNewToolDisplayLogic {
+    func displayAddToolViewState(_ viewState: AddNewTool.ViewState) {}
+}
+
+final class AddNewToolDisplayLogicSpy: AddNewToolDisplayLogic {
+
+    private(set) var displayAddToolViewStatePassedViewStates = [AddNewTool.ViewState]()
+
+    func displayAddToolViewState(_ viewState: AddNewTool.ViewState) {
+        displayAddToolViewStatePassedViewStates.append(viewState)
+    }
+}
