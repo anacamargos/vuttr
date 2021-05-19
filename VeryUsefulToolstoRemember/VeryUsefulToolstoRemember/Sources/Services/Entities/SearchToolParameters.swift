74ed95626603ca9685128a535b15e1ed5147c23d
anacamargos <alcvlara@sga.pucminas.br>
2021-05-19 19:22:28 -0300

    Creating new request

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Services/Entities/SearchToolParameters.swift
@@ -0,0 +1,13 @@
+//
+//  SearchToolParameters.swift
+//  VeryUsefulToolstoRemember
+//
+//  Created by Ana Leticia Camargos on 19/05/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import Foundation
+
+struct SearchToolParameters {
+    let text: String
+}
--- VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Services/ToolsRequest.swift
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Services/ToolsRequest.swift
@@ -13,12 +13,13 @@ enum ToolsRequest: NetworkRequest {
     case getTools
     case createNewTool(CreateNewToolParameters)
     case deleteTool(id: UInt)
+    case searchTools(SearchToolParameters)
 
     var baseURL: BaseURL { .serviceGroup(.vuttr) }
 
     var path: String? {
         switch self {
-        case .getTools, .createNewTool:
+        case .getTools, .createNewTool, .searchTools:
             return "/tools"
         case let .deleteTool(id):
             return "/tools/\(id)"
@@ -27,7 +28,7 @@ enum ToolsRequest: NetworkRequest {
 
     var method: HTTPMethod {
         switch self {
-        case .getTools:
+        case .getTools, .searchTools:
             return .get
         case .createNewTool:
             return .post
@@ -40,6 +41,15 @@ enum ToolsRequest: NetworkRequest {
         switch self {
         case let .createNewTool(parameters):
             return .json((try? parameters.toJSON()) ?? [:])
+        default:
+            return nil
+        }
+    }
+
+    var urlParameters: URLParameters? {
+        switch self {
+        case let .searchTools(parameters):
+            return .raw(["q": parameters.text])
         default:
             return nil
         }
