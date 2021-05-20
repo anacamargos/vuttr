d09317ce6f5b69534ce0325ebdff903f69815ec1
anacamargos <alcvlara@sga.pucminas.br>
2021-05-20 18:10:06 -0300

    Creating new use case

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Scenes/Home/Domain/UseCases/SearchForToolUseCase.swift
@@ -0,0 +1,63 @@
+//
+//  SearchForToolUseCase.swift
+//  VeryUsefulToolstoRemember
+//
+//  Created by Ana Leticia Camargos on 20/05/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import Foundation
+
+enum SearchForToolUseCaseError: Error {
+    case genericError
+}
+
+protocol SearchForToolUseCaseProvider {
+    func execute(request: SearchForToolUseCaseModels.Request, then handle: @escaping (Result<[GetUsefulToolsUseCaseModels.Tool], SearchForToolUseCaseError>) -> Void)
+}
+
+enum SearchForToolUseCaseModels {
+
+    struct Request {
+        let text: String
+    }
+}
+
+final class SearchForToolUseCase: SearchForToolUseCaseProvider {
+
+    // MARK: - Dependencies
+
+    private let service: ToolsServicesProvider
+
+    // MARK: - Initializer
+
+    init(service: ToolsServicesProvider) {
+        self.service = service
+    }
+
+    // MARK: - SearchForToolUseCaseProvider
+
+    func execute(request: SearchForToolUseCaseModels.Request, then handle: @escaping (Result<[GetUsefulToolsUseCaseModels.Tool], SearchForToolUseCaseError>) -> Void) {
+        let parameters = SearchToolParameters(text: request.text)
+        service.searchForTool(parameters: parameters) { [weak self] result in
+            switch result {
+            case let .success(data):
+                self?.handleSuccess(input: data, then: handle)
+            case .failure:
+                handle(.failure(.genericError))
+            }
+        }
+    }
+
+    // MARK: - Private Methods
+
+    private func handleSuccess(
+        input: [ToolResponseEntity],
+        then handle: @escaping (Result<[GetUsefulToolsUseCaseModels.Tool], SearchForToolUseCaseError>) -> Void
+    ) {
+        let domainTools = input.map {
+            GetUsefulToolsUseCaseModels.Tool(id: $0.id, title: $0.title, link: $0.link, description: $0.description, tags: $0.tags)
+        }
+        handle(.success(domainTools))
+    }
+}
