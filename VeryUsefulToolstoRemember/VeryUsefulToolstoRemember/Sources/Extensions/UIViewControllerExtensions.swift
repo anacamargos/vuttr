9231259294d6fd2fb0184ad3f297fba7270fbb39
anacamargos <alcvlara@sga.pucminas.br>
2021-07-01 18:06:36 -0300

    Creating new extension

---
+++ VeryUsefulToolstoRemember/VeryUsefulToolstoRemember/Sources/Extensions/UIViewControllerExtensions.swift
@@ -0,0 +1,33 @@
+//
+//  UIViewControllerExtensions.swift
+//  VeryUsefulToolstoRemember
+//
+//  Created by Ana Leticia Camargos on 01/07/21.
+//  Copyright © 2021 Ana Letícia Camargos. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    private enum Constants {
+        static var alertCornerRadius: CGFloat = 15.0
+        static var attributedTitle: String = "attributedTitle"
+    }
+    
+    func showToast(message: String, seconds: Double = 3.0) {
+        let alert = UIAlertController(title: message, message: nil, preferredStyle: .actionSheet)
+        alert.view.layer.cornerRadius = Constants.alertCornerRadius
+        let titleFont = [
+            NSAttributedString.Key.font: UIFont.themeFont(for: .subtitle, weight: .semibold),
+            NSAttributedString.Key.foregroundColor: UIColor.red
+        ]
+        let titleAttrStirng = NSMutableAttributedString(string: message, attributes: titleFont)
+        alert.setValue(titleAttrStirng, forKey: Constants.attributedTitle)
+
+        present(alert, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds) {
+            alert.dismiss(animated: true)
+        }
+    }
+}
